e99e0e304492bb85899f28faf89121d55c9289ed
Nick Vigier <nvigier46@gmail.com>
2024-06-03 10:45:34 +0100

    test

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/:path*",
+        source: "/api/python/:path*",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+            : "/api/python",
       },
       {
         source: "/docs",
